58744d7f7037a68fe58b890d6cf90e0770d07e51
Joffrey Bion <joffrey.bion@gmail.com>
2023-05-03 17:49:15 +0200

    Upgrade Kotlin to 1.8.21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation(gradleApi())
     implementation(gradleKotlinDsl())
 
-    implementation(kotlin("gradle-plugin", "1.8.0"))
+    implementation(kotlin("gradle-plugin", "1.8.21"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
 
     implementation("org.hildan.gradle:gradle-kotlin-publish-plugin:1.0.0")
