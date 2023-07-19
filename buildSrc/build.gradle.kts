8a43a2a7c4fc6e007b9d8f42846e6f8d2bbe224c
Joffrey Bion <joffrey.bion@gmail.com>
2023-07-19 14:23:33 +0200

    Upgrade Kotlin publish plugin to 1.1.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,6 +14,6 @@ dependencies {
     implementation(kotlin("gradle-plugin", "1.9.0"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
 
-    implementation("org.hildan.gradle:gradle-kotlin-publish-plugin:1.0.0")
+    implementation("org.hildan.gradle:gradle-kotlin-publish-plugin:1.1.1")
     implementation("ru.vyarus:gradle-github-info-plugin:1.5.0")
 }
